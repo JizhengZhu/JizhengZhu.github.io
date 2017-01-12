18eb420a894d6f30014fc765c91831d9e7d763ff
FoxconnJim <1303610866@qq.com>
2017-01-13 01:38:15 +0800

    改UI

--- js/getluckyforemployees.js
+++ js/getluckyforemployees.js
@@ -1,70 +1,51 @@
 //定义一个数组变量存放几个数据，一个定时器，一个标识变量
 var data=[
-'彭金萍',
-'吕增辉',
-'吕绍枫',
-'朱巧云',
-'张文君',
-'曹琳娇',
-'吕衡',
-'李希马妍',
-'戴梦',
-'姚艳',
-'张翼',
-'黄馨雅',
-'黄远',
-'李玉隽',
-'张杰',
-'张泽凯',
-'黄漫妮',
-'吴梓瑶',
-'吴欣彤',
-'冯文锋',
-'赖志军',
-'赖熙元',
-'欧恩',
-'欧政',
-'金兰',
-'谢军',
-'谢子萱',
-'许嘉骏',
-'黄燕',
-'叶靖豪',
-'叶洁容',
-'叶宇熙',
-'魏小琴',
-'曾超炜',
-'曾雨柔',
-'胡晓妹',
-'吴静',
-'游媛媛',
-'高梦真',
-'李孔炼',
-'李秀玲',
-'马人凤',
-'曹永明',
-'曾欣昕',
-'周鹏',
-'麦宝文',
-'繆潘',
-'王秀伟',
-'辛志勇',
-'邓文祥',
-'张碧宏',
-'王彬',
-'周福伟',
-'邓云华',
-'金春苗',
-'罗鹏',
-'陶玲',
-'甘铭祥',
-'陈家荣',
-'任伟峰',
-'任灏钊',
-'曾文慧',
-'曹炜棠'],
+'叶林华',
+'黎滔',
+'黄军',
+'邓海燕',
+'邵肖凤',
+'陈小惠',
+'陈思捷',
+'杨泳婷',
+'曾小明',
+'叶佩钗',
+'叶得荣',
+'吴碧婵',
+'李梦慧',
+'苏杰',
+'向丽群',
+'沈志荣',
+'彭冉',
+'蒋长生',
+'梁文娟',
+'黎才家',
+'王世坤',
+'曹宝详',
+'王功喜',
+'舒易均',
+'王昌政',
+'王昌由',
+'黄尚高',
+'李汉桂',
+'邹爱辉',
+'梁宝霞',
+'陆德胜',
+'曾文峰',
+'周冬梅',
+'朱瑛',
+'叶景安',
+'陈世军',
+'龙佩',
+'廖文海',
+'朱轶丽',
+'刘小蒿',
+'王清丽',
+'罗禹凡',
+'梁登昆'],
 	timer=null
 	flag=0
+	count = 0
 	fullscreen = 0;
 //封装一个方法拿到 通过class拿到的标签 注意拿到的时候是个数组对象，记得最后[0]，取一下第一个对象
 function getByClass(clsName,parent){
@@ -83,30 +64,102 @@ function getByClass(clsName,parent){
 window.onload=function(){
 	var ary=data;
 
-	var oTitle=getByClass('title')[0];
-
 	var random=0;
 
+	var oTitle=getByClass('title')[0];
+
+	var oLucker3_0=getByClass('lucker3_0')[0];
+	var oLucker3_1=getByClass('lucker3_1')[0];
+	var oLucker3_2=getByClass('lucker3_2')[0];
+	var oLucker3_3=getByClass('lucker3_3')[0];
+	var oLucker3_4=getByClass('lucker3_4')[0];
+	var oLucker3_5=getByClass('lucker3_5')[0];
+	var oLucker3_6=getByClass('lucker3_6')[0];
+	var oLucker3_7=getByClass('lucker3_7')[0];
+
+	var oLucker2_0=getByClass('lucker2_0')[0];
+	var oLucker2_1=getByClass('lucker2_1')[0];
+	var oLucker2_2=getByClass('lucker2_2')[0];
+	var oLucker2_3=getByClass('lucker2_3')[0];
+
+	var oLucker1_0=getByClass('lucker1_0')[0];
+	var oLucker1_1=getByClass('lucker1_1')[0];
+
+	var oLucker0_0=getByClass('lucker0_0')[0];
 	//键盘事件 针对的是整个document
 	document.onkeyup=fnkey;
 
 	function fnplay(){
+		count++;
+
 	//var that=this;//这里指的是begin这个按钮 这里暂时不考虑这个。
 	//每个开始之前关闭一下定时器，不然每次按click的时候容易加快速度，以至于整个浏览器容易奔溃  
+
 		clearInterval(timer);
 		//定义一个定时器
 		timer=setInterval(function(){
 			//Math.random()拿到的是0-1之前的数字，去乘数组的长度 再取整数可以拿到想要的数组下标
-			random=Math.floor(Math.random()*data.length); //floor去取整
+			random=Math.floor(Math.random()*ary.length); //floor去取整
 			//把拿到的数组的值写进去
-			oTitle.innerHTML=data[random];
+			oTitle.innerHTML = ary[random];
 		},50);
 	}
 
 	function fnstop(){
 		clearInterval(timer);
+
+		switch(count) {
+			case 1:
+				oLucker3_0.innerHTML=oTitle.innerHTML;
+				break;
+			case 2:
+				oLucker3_1.innerHTML=oTitle.innerHTML;
+				break;
+			case 3:
+				oLucker3_2.innerHTML=oTitle.innerHTML;
+				break;
+			case 4:
+				oLucker3_3.innerHTML=oTitle.innerHTML;
+				break;
+			case 5:
+				oLucker3_4.innerHTML=oTitle.innerHTML;
+				break;
+			case 6:
+				oLucker3_5.innerHTML=oTitle.innerHTML;
+				break;
+			case 7:
+				oLucker3_6.innerHTML=oTitle.innerHTML;
+				break;
+			case 8:
+				oLucker3_7.innerHTML=oTitle.innerHTML;
+				break;
+			case 9:
+				oLucker2_0.innerHTML=oTitle.innerHTML;
+				break;
+			case 10:
+				oLucker2_1.innerHTML=oTitle.innerHTML;
+				break;
+			case 11:
+				oLucker2_2.innerHTML=oTitle.innerHTML;
+				break;
+			case 12:
+				oLucker2_3.innerHTML=oTitle.innerHTML;
+				break;
+			case 13:
+				oLucker1_0.innerHTML=oTitle.innerHTML;
+				break;
+			case 14:
+				oLucker1_1.innerHTML=oTitle.innerHTML;
+				break;
+			case 15:
+				oLucker0_0.innerHTML=oTitle.innerHTML;
+				break;
+			default:
+				break;
+		}
 		ary.splice(random,1);
 	}
+
 
 	function fnkey(event){
 		console.log(event.keyCode);
@@ -152,6 +205,8 @@ window.onload=function(){
 	    	element.webkitRequestFullScreen();  
 	  	}  
 	}  
+  
+
 }
 
 
